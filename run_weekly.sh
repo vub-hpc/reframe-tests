02337d3830b3a68a24668672367e628b4123cd55
Samuel Moors <samuel.moors@vub.be>
2024-06-04 12:22:44 +0200

    return non-zero exit code for any failed tests

--- run_weekly.sh
+++ run_weekly.sh
@@ -2,18 +2,27 @@
 
 cd $(dirname "$0")
 
-# shared storage test
-./run.sh -c ior --partitions skylake-mn-mpi-ib
-./run.sh -c ior --partitions skylake-mn-mpi-eth
+cmds=(
+    # shared storage test
+    "./run.sh -c ior --partitions skylake-mn-mpi-ib"
+    "./run.sh -c ior --partitions skylake-mn-mpi-eth"
 
-# multi-node tests
-./run.sh -c osu
-./run.sh -c gromacs_bench -n GMXBenchMEMMultiNode
-./run.sh -c cp2k_tests -n CP2KTestMultiNode
+    # multi-node tests
+    "./run.sh -c osu"
+    "./run.sh -c gromacs_bench -n GMXBenchMEMMultiNode"
+    "./run.sh -c cp2k_tests -n CP2KTestMultiNode"
 
-# single-node tests
-./run.sh -c blas-tester
-./run.sh -c gromacs_bench -n GMXBenchMEMSingleNode
-./run.sh -c gromacs_bench -n GMXBenchMEMSingleNodeGPU
-./run.sh -c cp2k_tests -n CP2KTestSingleNode
+    # single-node tests
+    "./run.sh -c blas-tester"
+    "./run.sh -c gromacs_bench -n GMXBenchMEMSingleNode"
+    "./run.sh -c gromacs_bench -n GMXBenchMEMSingleNodeGPU"
+    "./run.sh -c cp2k_tests -n CP2KTestSingleNode"
+)
 
+for cmd in "${cmds[@]}"; do
+    eval "$cmd"
+    exitcode=$?
+    ((total+=exitcode))
+done
+
+exit $total
